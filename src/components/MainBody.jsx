7b3ff57706181991be8972b9d146826ad1ca2397
Abdirazakf <farahabdirazak13@gmail.com>
2025-10-02 21:15:35 -0400

    Setup main body with cv container and added form files

---
+++ src/components/MainBody.jsx
@@ -0,0 +1,43 @@
+import { useState } from 'react'
+import GeneralInfo from './forms/GeneralInfo'
+import Education from './forms/Education'
+import Experience from './forms/Experience'
+
+export default function Main() {
+    const [cvData, setData] = useState({
+        generalInfo: {
+            fullName: '',
+            email: '',
+            phoneNumber: '',
+            city: '',
+            state: '',
+            linkedin: ''
+        },
+        education: [{
+            id: 1,
+            school: '',
+            field: '',
+            degree: '',
+            gradDate: ''
+        }],
+        experience: [{
+            id: 1,
+            company: '',
+            position: '',
+            responsibilities: [],
+            startDate: '',
+            endDate: ''
+        }]
+    })
+
+    return (
+        <main>
+            <h2>Enter Your Info</h2>
+            <div className="cv-container">
+                <GeneralInfo />
+                <Education />
+                <Experience />
+            </div>
+        </main>
+    )
+}
